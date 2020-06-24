4cff118a54cfd62844a21e53d09fdb17c2cd55ef
wupeng <17327767735@163.com>
2020-06-24 17:56:38 +0800

    2020.06.24晚新增

--- page/page_system_manage/page_num_dict_manage.py
+++ page/page_system_manage/page_num_dict_manage.py
@@ -18,8 +18,8 @@ class PageNumDictManage(Base):
         self.base_click(page.num_dict_manage_insert_click_button)
 
     # 输入描述
-    def page_num_dict_manage_desc_input(self):
-        self.base_input(page.public_desc_input, page.public_value)
+    def page_num_dict_manage_desc_input(self, desc):
+        self.base_input(page.public_desc_input, desc)
 
     # 点击是否系统字典
     def page_num_dict_manage_insert_isno_dict_click(self):
@@ -30,19 +30,21 @@ class PageNumDictManage(Base):
         self.base_click(page.num_dict_manage_insert_isno_dict_select)
 
     # 输入类型
-    def page_num_dict_manage_type_input(self):
-        self.base_input(page.public_type_input, page.public_value)
+    def page_num_dict_manage_type_input(self, type):
+        self.base_input(page.public_type_input, type)
+
     # 输入类型其他
-    def page_num_dict_manage_type_input_other(self):
-        self.base_input(page.public_type_input, page.public_value2)
+    def page_num_dict_manage_type_input_other(self, type_other):
+        self.base_input(page.public_type_input, type_other)
+
     # 输入备注信息
-    def page_num_dict_manage_remarks_input(self):
-        self.base_input(page.public_remarks_input, page.public_value)
+    def page_num_dict_manage_remarks_input(self, mark_info):
+        self.base_input(page.public_remarks_input, mark_info)
 
     # 点击保存
     def page_num_dict_manage_insert_save_button(self):
         self.base_click(page.public_insert_save_button)
-
+    
     """
         数字字典管理修改
     """
@@ -64,24 +66,24 @@ class PageNumDictManage(Base):
         self.base_click(page.num_dict_manage_four_line_dict_nape_insert_button)
 
     # 输入数据值
-    def page_num_dict_manage_four_line_value_input(self):
-        self.base_input(page.public_value_input, page.public_value)
+    def page_num_dict_manage_four_line_value_input(self, data):
+        self.base_input(page.public_value_input, data)
 
     # 输入标签名
-    def page_num_dict_manage_four_line_label_input(self):
-        self.base_input(page.public_label_input, page.public_value)
+    def page_num_dict_manage_four_line_label_input(self, tag):
+        self.base_input(page.public_label_input, tag)
 
     # 输入描述
-    def page_num_dict_manage_four_line_desc_input(self):
-        self.base_input(page.public_desc_input, page.public_value)
+    def page_num_dict_manage_four_line_desc_input(self, desc):
+        self.base_input(page.public_desc_input, desc)
 
     # 输入排序
-    def page_num_dict_manage_four_line_sort_input(self):
-        self.base_input(page.public_sort_input, page.public_order_num)
+    def page_num_dict_manage_four_line_sort_input(self, order):
+        self.base_input(page.public_sort_input, order)
 
     # 输入备注信息
-    def page_num_dict_manage_four_line_remarks_input(self):
-        self.base_input(page.public_remarks_input, page.public_value)
+    def page_num_dict_manage_four_line_remarks_input(self, mark_info):
+        self.base_input(page.public_remarks_input, mark_info)
 
     # 点击保存
     def page_num_dict_manage_four_line_insert_button(self):
@@ -100,51 +102,51 @@ class PageNumDictManage(Base):
     """
 
     # 数字字典管理新增
-    def page_num_dict_manage_insert(self):
+    def page_num_dict_manage_insert(self, desc, type_other, mark_info):
         self.page_num_dict_manage_click_button()
         self.page_num_dict_manage_insert_click_button()
-        self.page_num_dict_manage_desc_input()
+        self.page_num_dict_manage_desc_input(desc)
         self.page_num_dict_manage_insert_isno_dict_click()
         self.page_num_dict_manage_insert_isno_dict_select()
-        self.page_num_dict_manage_type_input_other()
-        self.page_num_dict_manage_remarks_input()
+        self.page_num_dict_manage_type_input_other(type_other)
+        self.page_num_dict_manage_remarks_input(mark_info)
         self.page_num_dict_manage_insert_save_button()
         time.sleep(2)
 
     # 数字字典管理编辑
-    def page_num_dict_manage_edit(self):
+    def page_num_dict_manage_edit(self, desc, type, mark_info):
         self.page_num_dict_manage_click_button()
         self.page_num_dict_manage_four_line_edit_button()
-        self.page_num_dict_manage_desc_input()
+        self.page_num_dict_manage_desc_input(desc)
         self.page_num_dict_manage_insert_isno_dict_click()
         self.page_num_dict_manage_insert_isno_dict_select()
-        self.page_num_dict_manage_type_input()
-        self.page_num_dict_manage_remarks_input()
+        self.page_num_dict_manage_type_input(type)
+        self.page_num_dict_manage_remarks_input(mark_info)
         self.page_num_dict_manage_insert_save_button()
         time.sleep(2)
 
     # 数字字典四行字典项新增
-    def page_num_dict_manage_four_line_nape_insert(self):
+    def page_num_dict_manage_four_line_nape_insert(self, data, tag, desc, order, mark_info):
         self.page_num_dict_manage_click_button()
         self.page_num_dict_manage_four_line_dict_nape_button()
         self.page_num_dict_manage_four_line_dict_nape_insert_button()
-        self.page_num_dict_manage_four_line_value_input()
-        self.page_num_dict_manage_four_line_label_input()
-        self.page_num_dict_manage_four_line_desc_input()
-        self.page_num_dict_manage_four_line_sort_input()
-        self.page_num_dict_manage_four_line_remarks_input()
+        self.page_num_dict_manage_four_line_value_input(data)
+        self.page_num_dict_manage_four_line_label_input(tag)
+        self.page_num_dict_manage_four_line_desc_input(desc)
+        self.page_num_dict_manage_four_line_sort_input(order)
+        self.page_num_dict_manage_four_line_remarks_input(mark_info)
         self.page_num_dict_manage_four_line_insert_button()
         time.sleep(2)
 
     # 数字字典四行字典项编辑
-    def page_num_dict_manage_four_line_nape_edit(self):
+    def page_num_dict_manage_four_line_nape_edit(self, data, tag, desc, order, mark_info):
         self.page_num_dict_manage_click_button()
         self.page_num_dict_manage_four_line_dict_nape_button()
         self.page_num_dict_manage_four_line_nape_edit_button()
-        self.page_num_dict_manage_four_line_value_input()
-        self.page_num_dict_manage_four_line_label_input()
-        self.page_num_dict_manage_four_line_desc_input()
-        self.page_num_dict_manage_four_line_sort_input()
-        self.page_num_dict_manage_four_line_remarks_input()
+        self.page_num_dict_manage_four_line_value_input(data)
+        self.page_num_dict_manage_four_line_label_input(tag)
+        self.page_num_dict_manage_four_line_desc_input(desc)
+        self.page_num_dict_manage_four_line_sort_input(order)
+        self.page_num_dict_manage_four_line_remarks_input(mark_info)
         self.page_num_dict_manage_four_line_insert_button()
         time.sleep(2)
--- scripts/system_manage_script/test_num_dict_manage.py
+++ scripts/system_manage_script/test_num_dict_manage.py
@@ -1,6 +1,11 @@
 import unittest
 from page.page_system_manage.page_num_dict_manage import PageNumDictManage
 from base.get_driver import GetDriver
+from parameterized import parameterized
+from tools.read_txt import read_txt
+import page
+import pytest
+import allure
 
 
 class TestNumDictManage(unittest.TestCase):
@@ -10,27 +15,50 @@ class TestNumDictManage(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.driver = GetDriver().get_driver()
         cls.pndm = PageNumDictManage(cls.driver)
-        cls.pndm.system_login("admin", 123456)
+        cls.pndm.system_login()
 
     @classmethod
     def tearDownClass(cls) -> None:
         GetDriver().quit_driver()
 
     # 测试数字字典管理新增
+    @parameterized.expand(read_txt("system_manage/system_num_dict.txt"))
+    @allure.step(title="测试数字字典的异常输入新增")
+    @pytest.mark.run(order=1)
+    def test_num_dict_manage_except_insert(self, desc, type_other, mark_info):
+        self.pndm.page_num_dict_manage_insert(desc, type_other, mark_info)
+
+    # 测试数字字典管理新增
+    @allure.step(title="测试数字字典的正常输入新增")
+    @pytest.mark.run(order=2)
     def test_num_dict_manage_insert(self):
-        self.pndm.page_num_dict_manage_insert()
+        self.pndm.page_num_dict_manage_insert(page.public_value, page.public_value, page.public_value)
 
     # 测试数字字典管理编辑
-    def test_num_dict_manage_edit(self):
-        self.pndm.page_num_dict_manage_edit()
+    @allure.step(title="测试数字字典的编辑")
+    @pytest.mark.run(order=3)
+    def test_num_dict_manage_edit(self, desc, type, mark_info):
+        self.pndm.page_num_dict_manage_edit(desc, type, mark_info)
 
     # 测试数字字典字典项新增
+    @parameterized.expand(read_txt("system_manage/system_num_dict_tem.txt"))
+    @allure.step(title="测试数字字典项的异常输入新增")
+    @pytest.mark.run(order=4)
+    def test_num_dict_manage_four_line_nape_insert(self, data, tag, desc, order, mark_info):
+        self.pndm.page_num_dict_manage_four_line_nape_insert(data, tag, desc, order, mark_info)
+
+    # 测试数字字典字典项新增
+    @allure.step(title="输入数字字典项的正常输入新增")
+    @pytest.mark.run(order=5)
     def test_num_dict_manage_four_line_nape_insert(self):
-        self.pndm.page_num_dict_manage_four_line_nape_insert()
+        self.pndm.page_num_dict_manage_four_line_nape_insert(page.public_value, page.public_value, page.public_value,
+                                                             page.public_order_num, page.public_value)
 
     # 测试数字字典字典项编辑
-    def test_num_dict_manage_four_line_nape_edit(self):
-        self.pndm.page_num_dict_manage_four_line_nape_edit()
+    @allure.step(title="输入数字字典项的编辑")
+    @pytest.mark.run(order=6)
+    def test_num_dict_manage_four_line_nape_edit(self, data, tag, desc, order, mark_info):
+        self.pndm.page_num_dict_manage_four_line_nape_edit(data, tag, desc, order, mark_info)
 
 
 if __name__ == '__main__':
